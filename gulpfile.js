19f9ede5c15374ec890856e078066fede65f472f
Carlosf96 <carlos.f.roque96@gmail.com>
2019-07-18 13:49:50 -0500

    Set up a live reload using browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 gulp.task('hello', function(done){
   console.log('hello from gulp');
@@ -11,11 +12,26 @@ gulp.task('sass', function(){//task to compile sass to css
   return gulp.src('src/scss/*.scss')//source
   .pipe(sass())//gulp-sass plugin to compile sass to css
   .pipe(autoprefixer())//gulp-autoprefixer plugin for vendor free css
+  .pipe(browserSync.reload{
+    stream: true
+  })
   .pipe(gulp.dest('src/css'))//output
 });
 
+gulp.task('serve', function(){
+  browserSync.init({
+    server: 'src',
+    port: 4000
+  });
+});//serves our assets on given port
 
+gulp.task('reload', function(done){
+  browserSync.reload();
+  done();
+})//browser reload function
 
 gulp.task('watch', function(){
-  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));//watches for changes and executes a function on change
+  gulp.watch('src/scss/**/*.scss', gulp.series('sass', 'reload'));//watches for changes and executes a function on change
 });//auto compiles sass to css on change
+
+gulp.task('live-server', gulp.series('serve', 'watch')); //live server function will create the live server that will serve and watch fo r any changes made to the sass file
