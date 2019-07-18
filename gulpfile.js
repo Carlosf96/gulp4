18339d55ad39b47037c65d95c045d165f3942b62
Carlosf96 <carlos.f.roque96@gmail.com>
2019-07-18 13:20:31 -0500

    Created task to compile sass to css using gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('hello', function(done){
   console.log('hello from gulp');
   done();
+});
+
+gulp.task('sass', function(){//task to compile sass to css
+  return gulp.src('src/scss/main.scss')//source
+  .pipe(sass())//plugin
+  .pipe(gulp.dest('src/css'))//output
 })
