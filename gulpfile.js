4b1b61be805c05f2b35b784479a33ab373326a18
Carlosf96 <carlos.f.roque96@gmail.com>
2019-07-18 13:32:28 -0500

    Added watch task to automate sass to css compilation

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,13 @@ gulp.task('hello', function(done){
 });
 
 gulp.task('sass', function(){//task to compile sass to css
-  return gulp.src('src/scss/main.scss')//source
+  return gulp.src('src/scss/*.scss')//source
   .pipe(sass())//plugin
   .pipe(gulp.dest('src/css'))//output
-})
+});
+
+
+
+gulp.task('watch', function(){
+  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));//watches for changes and executes a function on change
+});//auto compiles sass to css on change
